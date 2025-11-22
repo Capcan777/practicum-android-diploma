4f790ef457a28ff21c003f3d4686afed531bdc30
Capcan <lawyer@shultzlaw.ru>
2025-11-23 00:35:40 +0300

    gh-13: добавил базу, ентити, дао Добавил аннотацию для дао.

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 data class VacancyEntity(
     // Добавить поля для хранения информации о вакансии
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+    val id: Int,
     val title: String,
     val description: String,
     val salary: String,
