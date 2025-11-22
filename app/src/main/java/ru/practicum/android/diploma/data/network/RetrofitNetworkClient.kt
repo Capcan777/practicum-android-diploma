81b439537a6c0acdb6e74b8e36e34268d184a3ff
Capcan <lawyer@shultzlaw.ru>
2025-11-22 21:49:44 +0300

    gh-5 Создал компонент для проверки подключения к интернету: InternetConnectionStatus.kt Исправил пробелы в коде.

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -1,16 +1,12 @@
 package ru.practicum.android.diploma.data.network
 
 import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import ru.practicum.android.diploma.data.dto.Response
+import ru.practicum.android.diploma.util.InternetConnectionStatus
 
 class RetrofitNetworkClient(
     private val api: VacancyApi,
-    private val context: Context,
-    private val connectivityManager: ConnectivityManager
+    private val context: Context
 ) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
         if (!isConnected()) {
@@ -23,23 +19,10 @@ class RetrofitNetworkClient(
             result = 0
         }
 
-
     }
 
     private fun isConnected(): Boolean {
-        val connectivityManager = context.getSystemService(
-            Context.CONNECTIVITY_SERVICE
-        ) as ConnectivityManager
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
-            }
-        }
-        return false
+        return InternetConnectionStatus.isInternetAvailable(context)
     }
 }
 
--- app/src/main/java/ru/practicum/android/diploma/data/network/VacancyApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/VacancyApi.kt
@@ -8,15 +8,16 @@ import retrofit2.http.QueryMap
 interface VacancyApi {
     @GET("vacancies")
     suspend fun getVacancies(
-        @Header("Authorization") token: String, @Header("Content-Type") contentType: String,
+        @Header("Authorization") token: String,
+        @Header("Content-Type") contentType: String,
         @QueryMap(encoded = true) filters: Map<String, String>
     ): Any // Заменить Any на нужный тип ответа
 
     @GET("vacancies/{vacancyId}")
     suspend fun getVacancyById(
-        @Header("Authorization") token: String, @Header("Content-Type") contentType: String,
+        @Header("Authorization") token: String,
+        @Header("Content-Type") contentType: String,
         @Path("vacancyId") vacancyId: String
     ): Any // Заменить Any на нужный тип ответа
-
 
 }
