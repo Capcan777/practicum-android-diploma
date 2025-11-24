13d08e8530fc1ed74fee94db3f8eb1733bd8e018
yum3000 <fooco800@gmail.com>
2025-11-24 23:15:41 +0300

    Добавление NavGraph

---
+++ app/src/main/java/ru/practicum/android/diploma/navigation/NavGraph.kt
@@ -0,0 +1,38 @@
+package ru.practicum.android.diploma.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.practicum.android.diploma.ui.about.AboutTeamScreen
+import ru.practicum.android.diploma.ui.details.VacancyDetailsScreen
+import ru.practicum.android.diploma.ui.favourites.FavouritesVacanciesScreen
+import ru.practicum.android.diploma.ui.filter.FilterSettingsScreen
+import ru.practicum.android.diploma.ui.search.SearchScreen
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.Search.route) {
+
+        composable(Routes.Search.route) { SearchScreen() }
+        composable(Routes.VacancyDetails.route) { backStackEntry ->
+            val vacancyId = backStackEntry.arguments?.getString("vacancyId") ?: return@composable
+            VacancyDetailsScreen(vacancyId)
+        }
+        composable(Routes.Favourites.route) { FavouritesVacanciesScreen() }
+        composable(Routes.SettingsFilter.route) { FilterSettingsScreen() }
+        composable(Routes.About.route) { AboutTeamScreen() }
+    }
+}
+
+
+sealed class Routes(val route: String) {
+    object Search : Routes("search")
+    object VacancyDetails : Routes("vacancy_details/{vacancyId}") {
+        fun createRoute(vacancyId: String) = "vacancy_details/$vacancyId"
+    }
+    object Favourites : Routes("favourites")
+    object SettingsFilter : Routes("filter")
+    object About : Routes("about")
+}
