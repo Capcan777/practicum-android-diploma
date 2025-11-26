56c2612109fbb9305518c4bd28699b59e9518be7
yum3000 <fooco800@gmail.com>
2025-11-26 21:03:00 +0300

    Коды состояния HTTP выведены в util

--- app/src/main/java/ru/practicum/android/diploma/data/VacancyRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/VacancyRepositoryImpl.kt
@@ -8,6 +8,7 @@ import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.domain.DomainMapper
 import ru.practicum.android.diploma.domain.api.VacancyRepository
 import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.util.ResponseCodes
 
 class VacancyRepositoryImpl(
     private val networkClient: NetworkClient,
@@ -16,7 +17,7 @@ class VacancyRepositoryImpl(
     override suspend fun searchVacancies(request: SearchRequest): Flow<List<Vacancy>> = flow {
         val response = networkClient.doRequest(request)
         when (response.result) {
-            200 -> {
+            ResponseCodes.SUCCESS -> {
                 val vacancies = (response as SearchResponse).vacancies.map { vacancyDto ->
                     mapper.mapVacancy(vacancyDto)
                 }
---
+++ app/src/main/java/ru/practicum/android/diploma/util/ResponseCodes.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.util
+
+object ResponseCodes {
+    const val SUCCESS = 200
+    const val ERROR_CLIENT = 400
+    const val ERROR_SERVER = 500
+}
