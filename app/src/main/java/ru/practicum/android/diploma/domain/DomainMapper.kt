fe78e2ebb47475b7263b5d06a1dfc4b91af2e0f7
Ilya Drugov <thehardkor@yandex.ru>
2025-11-28 20:05:56 +0300

    добавил пагинацию

--- app/src/main/java/ru/practicum/android/diploma/domain/DomainMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/DomainMapper.kt
@@ -3,10 +3,12 @@ package ru.practicum.android.diploma.domain
 import ru.practicum.android.diploma.data.dto.EmployerDto
 import ru.practicum.android.diploma.data.dto.FilterIndustryDto
 import ru.practicum.android.diploma.data.dto.SalaryDto
+import ru.practicum.android.diploma.data.dto.SearchResponse
 import ru.practicum.android.diploma.data.dto.VacancyItemDto
 import ru.practicum.android.diploma.domain.models.Employer
 import ru.practicum.android.diploma.domain.models.Industry
 import ru.practicum.android.diploma.domain.models.SalaryRange
+import ru.practicum.android.diploma.domain.models.SearchResult
 import ru.practicum.android.diploma.domain.models.Vacancy
 
 class DomainMapper {
@@ -42,6 +44,17 @@ class DomainMapper {
         return Industry(
             id = dto.id,
             name = dto.name
+        )
+    }
+
+    fun mapSearchResult(response: SearchResponse): SearchResult {
+        return SearchResult(
+            vacancies = response.items.map { vacancyDto ->
+                mapVacancy(vacancyDto)
+            },
+            currentPage = response.page,
+            totalPages = response.pages,
+            found = response.found
         )
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/SearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/SearchInteractor.kt
@@ -2,8 +2,10 @@ package ru.practicum.android.diploma.domain
 
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.dto.SearchRequest
-import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.SearchResult
 
 interface SearchInteractor {
-    suspend fun searchVacancies(request: SearchRequest): Flow<List<Vacancy>?>
+    suspend fun searchVacancies(request: SearchRequest): Flow<SearchResult?>
+
+    suspend fun loadNextPage(query: String, nextPage: Int): Flow<SearchResult?>
 }
