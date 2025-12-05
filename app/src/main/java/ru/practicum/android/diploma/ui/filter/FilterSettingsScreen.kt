464f012ba3f8a807a78ef67cdb9117b282ed6a65
Capcan <lawyer@shultzlaw.ru>
2025-12-06 01:09:02 +0300

    GH-123: Верстка экрана "Фильтра"
    Took 5 hours 7 minutes

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterSettingsScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterSettingsScreen.kt
@@ -1,14 +1,183 @@
 package ru.practicum.android.diploma.ui.filter
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.designsystem.theme.VacancyTheme
+import ru.practicum.android.diploma.ui.common.AppBar
 
 @Composable
 fun FilterSettingsScreen(
-    navController: NavController
+    navController: NavController,
+    onBack: () -> Unit
 ) {
-    Text(text = stringResource(R.string.filter_settings))
+    var salaryText by remember { mutableStateOf("") }
+    var hideWithoutSalary by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            AppBar(
+                title = stringResource(R.string.filter_settings),
+                onBack = onBack,
+                actions = null
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(VacancyTheme.colorScheme.background)
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp)
+        ) {
+            FilterRow(
+                text = stringResource(R.string.place_of_work),
+                onClick = { } // без клика
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                FilterRow(
+                    text = stringResource(R.string.industry),
+                    onClick = { } // обработать клик на страницу отрасли
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = salaryText,
+                onValueChange = {
+                    if (salaryText.all { it.isDigit() }) {
+                        salaryText = it
+                    }
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.expected_salary),
+                        style = VacancyTheme.typography.regular12,
+                    )
+                },
+                trailingIcon = {
+                    if (salaryText.isNotEmpty()) {
+                        Icon(
+                            imageVector = Icons.Filled.Clear,
+                            contentDescription = null,
+                            tint = VacancyTheme.colorScheme.onPrimaryContainer,
+                            modifier = Modifier.clickable {
+                                salaryText = ""
+                                // обработка нажатия на кнопку очистки
+                            }
+                        )
+                    }
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(R.string.enter_amount),
+                        style = VacancyTheme.typography.regular16,
+                        color = VacancyTheme.colorScheme.secondary
+                    )
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = VacancyTheme.shapes.shape10dp,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = VacancyTheme.colorScheme.tertiaryContainer,
+                    unfocusedBorderColor = VacancyTheme.colorScheme.tertiaryContainer,
+                    cursorColor = VacancyTheme.colorScheme.primary,
+                    focusedContainerColor = VacancyTheme.colorScheme.tertiaryContainer,
+                    unfocusedContainerColor = VacancyTheme.colorScheme.tertiaryContainer,
+                    unfocusedLabelColor = VacancyTheme.colorScheme.inverseSurface,
+                    focusedLabelColor = VacancyTheme.colorScheme.primary,
+                )
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { hideWithoutSalary = !hideWithoutSalary },
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.do_not_show_without_salary),
+                    style = VacancyTheme.typography.regular16,
+                    color = VacancyTheme.colorScheme.onBackground
+                )
+                Checkbox(
+                    checked = hideWithoutSalary,
+                    onCheckedChange = { hideWithoutSalary = it },
+                    colors = CheckboxDefaults.colors(VacancyTheme.colorScheme.primary)
+
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun FilterRow(
+    text: String,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .height(60.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            style = VacancyTheme.typography.regular16,
+            color = VacancyTheme.colorScheme.onBackground
+        )
+        Icon(
+            imageVector = Icons.Filled.ChevronRight,
+            contentDescription = null,
+            tint = VacancyTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier.size(24.dp)
+        )
+    }
 }
